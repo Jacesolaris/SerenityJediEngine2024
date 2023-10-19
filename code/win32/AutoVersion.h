0838b3d7e16d16cbcd436c1140b12177d5fa6f41
Wood Family <woodfamily2487@hotmail.com>
2023-10-19 08:02:48 +0100

    "Year-23,Month-10,Day-19,BuildNum-12" Rend2 Day 1

--- code/win32/AutoVersion.h
+++ code/win32/AutoVersion.h
@@ -35,8 +35,8 @@ This file is part of Jedi Academy.
 #define VERSION_MINOR_RELEASE		10
 #define VERSION_INTERNAL_BUILD		19
 
-#define VERSION_STRING				"Year-23,Month-10,Day-19,BuildNum-10"
-#define VERSION_STRING_DOTTED		"Year-23,Month-10,Day-19,BuildNum-10" // build date
+#define VERSION_STRING				"Year-23,Month-10,Day-19,BuildNum-12"
+#define VERSION_STRING_DOTTED		"Year-23,Month-10,Day-19,BuildNum-12" // build date
 
 
 #endif // __AUTO_VERSION_HEADER
